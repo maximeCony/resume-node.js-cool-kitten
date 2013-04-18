6fb05008c6ca28232ba644b90835ea56d179fe80
Maxime cony <maxime.cony@gmail.com>
2013-04-17 17:34:46 -0700

    add more comments

--- server.js
+++ server.js
@@ -5,19 +5,23 @@ var express = require('express')
 
 var app = express();
 
+//mailing configuration
+var sendgrid = new SendGrid(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 
+//global configuration
 app.configure(function(){
 
+	//used to parse forms
 	app.use(express.bodyParser());
 
 	// serve static files
 	app.use("/public", express.static(__dirname + '/public'));
 
+	//set swig as view engine
 	app.engine('.html', cons.swig);
 	app.set('view engine', 'html');
 
-	// NOTE: Swig requires some extra setup
-	// This helps it know where to look for includes and parent templates
+	//swig configuration
 	swig.init({
 		root: __dirname + '/views',
 	    allowErrors: true // allows errors to be thrown and caught by express instead of suppressed by Swig
@@ -26,8 +30,7 @@ app.configure(function(){
 });
 
 
-var sendgrid = new SendGrid(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
-
+//get main page
 app.get('/', function(req, res){
 
 	res.render("index.html", { 
@@ -36,12 +39,16 @@ app.get('/', function(req, res){
 	
 });
 
+//post mail (on form submit)
 app.post('/mail', function(req, res){
 
+	//required name and message
 	if(req.body.name && req.body.message) {
 
+		//render mail view
 		app.render('email.html', { params: req.body }, function(err, html){
 			
+			//send the mail
 			sendgrid.send({
 				to: 'maxime.cony@gmail.com',
 				from: 'contact@maxime-cony.com',
@@ -55,10 +62,14 @@ app.post('/mail', function(req, res){
 				}
 			});
 		});
+		//redirect user to the main page
 		res.redirect('/');
 
 	} else {
+		//redirect user to the main page
 		res.location('/');
+
+		//render the wiew with fields content
 		res.render("index.html", { 
 			params: req.body,
 			scrollTo: 'formButtonSendMessage'
@@ -67,6 +78,7 @@ app.post('/mail', function(req, res){
 
 });
 
+//start listening
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log("Listening on " + port);
